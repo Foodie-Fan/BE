a29c515af947d36721ffc307a8ad455024f50a86
AlexFox1777 <mix.filipov@mail.ru>
2019-12-03 15:12:17 -0800

    latest code

--- db/seeds/01-users.js
+++ db/seeds/01-users.js
@@ -1,11 +1,10 @@
 
 exports.seed = function(knex) {
-  return knex('auth').del()
+  return knex('users').del()
     .then(function () {
-      return knex('auth').insert([
-        {id: 1, username: 'sasha', password: "sasha"},
-        {id: 2, username: 'vasilii', password: "vasilii"},
-        {id: 3, username: 'olya', password: "olya"},
+      return knex('users').insert([
+        {id: 1, username: 'sasha1777', name: "Aleksandra Foksman", password: "$2a$10$biVllna/1TTBELRNEanuJeDOvMNt0snFj8msY1bD1jGkU5Bb5LYAC", avatar: "http://res.cloudinary.com/krik13333/image/upload/v1571106602/f6obyrwmul9g53lq1gq7.jpg"},
+        {id: 2, username: 'nana1333', name: "Nana", password: "$2a$10$.U/jh1Wf5gjHZSAlfPqoN.TEjBEuJP.kKbc2LPyyM7UxE2ofvSSRm" , avatar: "https://res.cloudinary.com/krik13333/image/upload/v1570241223/il_fullxfull.1009061980_zajb_lwprk1.jpg"},
       ]);
     });
 };
